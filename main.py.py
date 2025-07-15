b1c11b8764136d24fabbd425da7aebc2dd4c2f6c
nznin <201469497+inznin@users.noreply.github.com>
2025-07-15 22:17:18 +0330

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -20,7 +20,9 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.edit_message_text(f"شما انتخاب کردید: {query.data}")
 
 if __name__ == '__main__':
+    print("ok")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.run_polling()
+
