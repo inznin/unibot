5df0e9c10821d3dffeeaae148a42ffa2eaf9c0a2
nznin <201469497+inznin@users.noreply.github.com>
2025-07-15 22:34:37 +0330

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -19,10 +19,9 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
     await query.edit_message_text(f"شما انتخاب کردید: {query.data}")
 
-if __name__ == '__main__':
-    print("ok")
+if __name__ == "__main__":
+    print("ربات در حال اجراست...")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.run_polling()
-
